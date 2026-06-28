182fce03dd374d1a02cee4ac5ed199ad189b7423
MrDeguitoo <diegoalfonsourb@gmail.com>
2026-06-27 21:02:34 -0400

    limpieza de ProveedorService

--- GestionInventario/proveedor-service/src/main/java/com/GestionInventario/proveedor_service/service/ProveedorService.java
+++ GestionInventario/proveedor-service/src/main/java/com/GestionInventario/proveedor_service/service/ProveedorService.java
@@ -2,19 +2,21 @@ package com.GestionInventario.proveedor_service.service;
 
 import com.GestionInventario.proveedor_service.model.Proveedor;
 import com.GestionInventario.proveedor_service.repository.ProveedorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProveedorService {
-    @Autowired
-    private ProveedorRepository proveedorRepository;
 
-    public List<Proveedor> listarTodos() { return proveedorRepository.findAll(); }
+    private final ProveedorRepository proveedorRepository;
+
+    public List<Proveedor> listarTodos() {
+        return proveedorRepository.findAll();
+    }
 
     public Proveedor buscarPorId(Long id) {
         return proveedorRepository.findById(id)
@@ -22,8 +24,12 @@ public class ProveedorService {
     }
 
     @Transactional
-    public Proveedor guardar(Proveedor proveedor) { return proveedorRepository.save(proveedor); }
+    public Proveedor guardar(Proveedor proveedor) {
+        return proveedorRepository.save(proveedor);
+    }
 
     @Transactional
-    public void eliminar(Long id) { proveedorRepository.deleteById(id); }
+    public void eliminar(Long id) {
+        proveedorRepository.deleteById(id);
+    }
 }
